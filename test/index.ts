8e06f21abc9e6a8941d56b3a43d8190fa62d2858
bouzuya <m@bouzuya.net>
2016-08-06 10:09:29 +0900

    remove add

--- test/index.ts
+++ test/index.ts
@@ -3,12 +3,10 @@ import * as assert from 'power-assert';
 import * as sinon from 'sinon';
 import * as proxyquire from 'proxyquire';
 
-import { add } from '../src/add';
-
 const { test } = beater();
 
 test('add', () => {
-  assert(add(1, 2) === 3);
+  assert(1 + 2 === 3);
   assert(sinon);
   assert(proxyquire);
 });
